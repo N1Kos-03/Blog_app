8dc80933240231177290a48629aa7f55476eecd0
Никита <kosmynin003@gmail.com>
2026-03-25 16:35:47 +0300

    ДЗ

--- app/page.tsx
+++ app/page.tsx
@@ -12,6 +12,38 @@ function Home() {
         readingTime="3 минуты"
         likes={4}
       />
+      <PostCard
+        category="Front-end"
+        date="1 месяц назад"
+        title="Как работать с CSS Grid"
+        description="Грид-раскладка (CSS Grid Layout) представляет собой двумерную систему сеток в CSS. Гриды подойдут и для верстки основных областей страницы.."
+        readingTime="3 минуты"
+        likes={4}
+      />
+      <PostCard
+        category="Front-end"
+        date="1 месяц назад"
+        title="Как работать с CSS Grid"
+        description="Грид-раскладка (CSS Grid Layout) представляет собой двумерную систему сеток в CSS. Гриды подойдут и для верстки основных областей страницы.."
+        readingTime="3 минуты"
+        likes={4}
+      />
+      <PostCard
+        category="Front-end"
+        date="1 месяц назад"
+        title="Как работать с CSS Grid"
+        description="Грид-раскладка (CSS Grid Layout) представляет собой двумерную систему сеток в CSS. Гриды подойдут и для верстки основных областей страницы.."
+        readingTime="3 минуты"
+        likes={4}
+      />
+      <PostCard
+        category="Front-end"
+        date="1 месяц назад"
+        title="Как работать с CSS Grid"
+        description="Грид-раскладка (CSS Grid Layout) представляет собой двумерную систему сеток в CSS. Гриды подойдут и для верстки основных областей страницы.."
+        readingTime="3 минуты"
+        likes={4}
+      />
     </div>
   );
 }
